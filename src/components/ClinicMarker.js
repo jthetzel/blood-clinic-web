cec583f3f52b7b06795944e2ae60f14dd861893e
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-05 17:05:59 -0230

    Add icons to markers

--- src/components/ClinicMarker.js
+++ src/components/ClinicMarker.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import IconButton from 'material-ui/IconButton'
+import ClinicIcon from 'material-ui/svg-icons/maps/local-hospital'
+
+const style = {
+  icon: {
+    height: 40,
+    width: 40,
+    textAlign: 'center',
+    display: 'inline-block'
+  }
+}
 
 const ClinicMarker = ({ text }) => (
-  <div>{text}</div>
+  <div>
+    <IconButton
+      tooltip={text}>
+      <ClinicIcon
+        color='red'
+        style={style.icon}
+        />
+    </IconButton>
+  </div>
 )
 
 export default ClinicMarker
