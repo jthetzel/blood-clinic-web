d07d97c28dc9615b726b14e4edaa2e707cc1733d
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-05 16:13:00 -0230

    Add clinic location information

---
+++ src/Constants.js
@@ -0,0 +1,26 @@
+export const clinics = [
+  {
+    name: 'Health Science Centre',
+    id: 'hs',
+    lat: 47.5719363,
+    lng: -52.7419408
+  },
+  {
+    name: 'Major\'s Path',
+    id: 'mp',
+    lat: 47.6102897,
+    lng: -52.7249336
+  },
+  {
+    name: 'Sr. Clare\'s Mercy',
+    id: 'sc',
+    lat: 47.5574587,
+    lng: -52.7215271
+  },
+  {
+    name: 'Waterford',
+    id: 'wf',
+    lat: 47.5287682,
+    lng: -52.7496391
+  }
+]
