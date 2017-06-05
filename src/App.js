aedcf80d444835e946c91749b39ac132490bfb9b
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-05 15:52:04 -0230

    Google-map-react example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
 import logo from './logo.svg'
 import './App.css'
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>
+
 class App extends Component {
+  static defaultProps = {
+    center: {lat: 59.95, lng: 30.33},
+    zoom: 9
+  }
   render () {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{height: '1000px'}}>
+        <GoogleMapReact
+          bootstrapURLKeys={{key: 'AIzaSyBR2VYaIyFFuQr_rcBY5Jc4dm5E9lOSIjY'}}
+          center={this.props.center}
+          zoom={this.props.zoom}
+          >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+            />
+        </GoogleMapReact>
       </div>
     )
   }
