b3e7487b713e29b6c884935d28b7c773ec55c48e
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-06 16:12:16 -0230

    Add current wait to drawer

--- src/components/ClinicDrawer.js
+++ src/components/ClinicDrawer.js
@@ -2,23 +2,27 @@ import React from 'react'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 
-const ClinicDrawer = ({ clinics, onClick }) => (
-  <Drawer
-    open
-    width={'25%'}
-    >
-    MetaClinic
-    {clinics.map((clinic, index) => {
-      return (
-        <MenuItem
-          key={index}
-          onTouchTap={() => onClick(clinic.id)}
-          >
-          {clinic.name}
-        </MenuItem>
-      )
-    })}
-  </Drawer>
-)
+const ClinicDrawer = ({ clinics, onClick, currentRate }) => {
+  return (
+    <Drawer
+      open
+      width={'30%'}
+      >
+      MetaClinic
+      {clinics.map((clinic, index) => {
+        const wait = currentRate[clinic.id]
+        const waitFormatted = (wait)? `${wait} minutes`: 'closed'
+        return (
+          <MenuItem
+            key={index}
+            onTouchTap={() => onClick(clinic.id)}
+            primaryText={clinic.name}
+            secondaryText={waitFormatted}
+            />
+        )
+      })}
+    </Drawer>
+  )
+}
 
 export default ClinicDrawer
