097a35a012e5abeb2f7ce61296acb9c02d0bb3b4
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-06 16:14:29 -0230

    Clean up ClinicModal

--- src/components/ClinicModal.js
+++ src/components/ClinicModal.js
@@ -1,44 +1,49 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import DayChart from '../charts/DayChart'
 
-/**
- * Dialog with action buttons. The actions are passed in as an array of React objects,
- * in this example [FlatButtons](/#/components/flat-button).
- *
- * You can also close this dialog by clicking outside the dialog, or with the 'Esc' key.
- */
 class ClinicModal extends React.Component {
   render () {
-    const { open, onRequestClose } = this.props
+    const { open, onRequestClose, selected, clinics } = this.props
+    const clinic = clinics.find(clinic => clinic.id === selected)
+    console.log(clinic)
+    if (!clinic) {
+      return (
+        <div />
+      )
+    }
+    const title = clinic.name || 'Clinic Wait Time'
+
     const actions = [
       <FlatButton
-        label='Cancel'
+        label='Close'
         primary
-        onTouchTap={onRequestClose}
-      />,
-      <FlatButton
-        label='Submit'
-        primary
-        keyboardFocused
         onTouchTap={onRequestClose}
       />
     ]
 
     return (
       <Dialog
-        title='Dialog With Actions'
+        title={title}
         actions={actions}
         modal={false}
         open={open}
         onRequestClose={onRequestClose}
         >
         <DayChart />
-        The actions in this window were passed in as an array of React objects.
       </Dialog>
     )
   }
 }
 
-export default ClinicModal
+const mapStateToProps = (state) => {
+  return {
+    selected: state.clinic.selected,
+    clinics: state.clinic.clinics,
+    currentRate: state.clinic.currentRate
+  }
+}
+
+export default connect(mapStateToProps, null)(ClinicModal)
