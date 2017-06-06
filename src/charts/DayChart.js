dcd701acb1e645bef4838b995f794224c905290e
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-06 18:55:53 -0230

    Clean up DayChart

--- src/charts/DayChart.js
+++ src/charts/DayChart.js
@@ -17,22 +17,21 @@ const DayChart = ({ dailyRate, selected }) => {
   let data = []
   dailyRate[selected].forEach(function (value, index) {
     if (value) {
-      data.push({hour: index, clinic: 'hs', wait: Number(value)})
+      data.push({hour: index, clinic: selected, wait: Number(value)})
     }
   })
-  console.log(data)
   return (
     <ResponsiveContainer minHeight={300}>
-    <AreaChart width={600} height={400} data={data}
-      margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-      <XAxis dataKey='hour' />
-      <YAxis />
-      <CartesianGrid strokeDasharray='3 3'/>
-      <Tooltip />
-      <Area type='monotone' dataKey='wait' stroke='#8884d8' fill='#8884d8'
-            isAnimationActive={false}
-            />
-    </AreaChart>
+      <AreaChart width={600} height={400} data={data}
+        margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+        <XAxis dataKey='hour' />
+        <YAxis />
+        <CartesianGrid strokeDasharray='3 3'/>
+        <Tooltip />
+        <Area type='monotone' dataKey='wait' stroke='#8884d8' fill='#8884d8'
+              isAnimationActive={false}
+              />
+      </AreaChart>
     </ResponsiveContainer>
   )
 }
