728d4a3e27158360cb462a4d76b0087c47d748a7
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-06 08:24:54 -0230

    Add clinic image urls

--- src/Constants.js
+++ src/Constants.js
@@ -3,24 +3,28 @@ export const clinics = [
     name: 'Health Science Centre',
     id: 'hs',
     lat: 47.5719363,
-    lng: -52.7419408
+    lng: -52.7419408,
+    image: 'https://bloodclinicserver.herokuapp.com/imgs/hs.jpg'
   },
   {
     name: 'Major\'s Path',
     id: 'mp',
     lat: 47.6102897,
-    lng: -52.7249336
+    lng: -52.7249336,
+    image: 'https://bloodclinicserver.herokuapp.com/imgs/mp.jpg'
   },
   {
     name: 'Sr. Clare\'s Mercy',
     id: 'sc',
     lat: 47.5574587,
-    lng: -52.7215271
+    lng: -52.7215271,
+    image: 'https://bloodclinicserver.herokuapp.com/imgs/sc.jpg'
   },
   {
     name: 'Waterford',
     id: 'wf',
     lat: 47.5287682,
-    lng: -52.7496391
+    lng: -52.7496391,
+    image: 'https://bloodclinicserver.herokuapp.com/imgs/wf.jpg'
   }
 ]
