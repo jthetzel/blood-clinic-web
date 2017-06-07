10eaedf0c3b16c098b554a7a7849dddb5a89ee01
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-07 08:43:54 -0230

    Add ClinicTimePicker

---
+++ src/components/ClinicTimePicker.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TimePicker from 'material-ui/TimePicker'
+
+const ClinicTimePicker = ({ value, onChange }) => {
+  return (
+    <TimePicker
+      autoOk
+      value={value}
+      onChange={(callback, date) => onChange(date)}
+      id={'ClinicDatePicker'}
+      />
+  )
+}
+
+export default ClinicTimePicker
