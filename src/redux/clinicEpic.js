b706020e8f9e59a0a046e1f8030a2d50c8fe9867
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-07 07:39:19 -0230

    Use redux datetime for clinicEpic

--- src/redux/clinicEpic.js
+++ src/redux/clinicEpic.js
@@ -3,19 +3,17 @@ import { ajax } from 'rxjs/observable/dom/ajax'
 import { ClinicTypes } from './clinicRedux'
 import { apiHost } from '../Constants'
 
-const date = new Date('2017-06-06T12:30:25.326Z')
-
 // updateClinicEpic
 const UPDATE_REQUESTED = ClinicTypes.UPDATE_REQUESTED
 const UPDATE_FULFILLED = ClinicTypes.UPDATE_COMPLETED
 export const updateFulfilled = payload => ({ type: UPDATE_FULFILLED, payload })
-export const updateClinicEpic = action$ =>
+export const updateClinicEpic = (action$, store) =>
       action$.ofType(UPDATE_REQUESTED)
       .mergeMap(action =>
                 ajax({
                   method: 'POST',
                   url: apiHost,
-                  body: JSON.stringify({date: date}),
+                  body: JSON.stringify({date: store.getState().datetime.datetime}),
                   headers: {
                     'Content-Type': 'application/json',
                   }
