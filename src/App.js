c1cb85466094b792dbac34d2b37ec24888479588
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-07 17:02:59 -0230

    Remove App.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux'
 import configureStore from './redux/configureStore'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import './App.css'
 import Map from './containers/Map'
 
 const store = configureStore()
