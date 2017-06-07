517c0097a7ab43b38f60cd033b45d70662f565ea
Jeremy Hetzel <jthetzel@openiii.com>
2017-06-07 11:58:03 -0230

    Improve date format

--- src/components/ClinicDatePicker.js
+++ src/components/ClinicDatePicker.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import DatePicker from 'material-ui/DatePicker'
+import moment from 'moment'
 
 const ClinicDatePicker = ({ value, onChange }) => {
   return (
@@ -8,6 +9,7 @@ const ClinicDatePicker = ({ value, onChange }) => {
       value={value}
       onChange={(callback, date) => onChange(date)}
       id={'ClinicDatePicker'}
+      formatDate={(date) => moment(date).format('dddd, LL')}
       />
   )
 }
