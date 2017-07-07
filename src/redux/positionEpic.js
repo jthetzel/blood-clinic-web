2cc5eb56de6e90ba616f71b808bac2dabdb524ff
Jeremy Hetzel <jthetzel@openiii.com>
2017-07-07 09:54:36 -0230

    Add positionEpic

---
+++ src/redux/positionEpic.js
@@ -0,0 +1,22 @@
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/map'
+import { PositionTypes } from './positionRedux'
+
+const POSITION_REQUESTED = PositionTypes.POSITION_REQUESTED
+const POSITION_RECEIVED = PositionTypes.POSITION_RECEIVED
+
+const positionReceived = position => ({ type: POSITION_RECEIVED, position })
+
+const getPosition = function (options) {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  })
+}
+
+export const updatePositionEpic = (action$, store) =>
+  action$.ofType(
+    POSITION_REQUESTED
+  )
+  .switchMap(action =>
+             getPosition().then((position) => positionReceived(position))
+            )
--- src/redux/root.js
+++ src/redux/root.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux'
 import { combineEpics } from 'redux-observable'
 import { updateClinicEpic } from './clinicEpic'
+import { updatePositionEpic } from './positionEpic'
 
 export const rootEpic = combineEpics(
-  updateClinicEpic
+  updateClinicEpic,
+  updatePositionEpic
 )
 
 export const rootReducer = combineReducers({
   map: require('./mapRedux').reducer,
   clinic: require('./clinicRedux').reducer,
   datetime: require('./datetimeRedux').reducer,
-  location: require('./locationRedux').reducer
+  position: require('./positionRedux').reducer
 })
