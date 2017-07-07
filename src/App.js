8088b7897a991ae7cb0db3ccbbb97612d9c13c3b
Jeremy Hetzel <jthetzel@openiii.com>
2017-07-07 15:06:43 -0230

    Initialize google analytics and pageview

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { Provider } from 'react-redux'
 import configureStore from './redux/configureStore'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import ReactGA from 'react-ga'
 import Map from './containers/Map'
+
+ReactGA.initialize('UA-102217061-1')
+ReactGA.pageview('/')
 
 const store = configureStore()
 // work around for material-ui tap events
